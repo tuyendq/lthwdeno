aace8938bf290939bf98d6ed7ba4fe99c525ccef
Tuyen Dang <tuyendq@gmail.com>
2020-06-09 10:42:50 +0700

    Dockerfile: comment testing RUN

--- deno-deploy/server.ts
+++ deno-deploy/server.ts
@@ -5,5 +5,5 @@ const server = serve({ port: 8000 });
 console.log("Now serving on port 8000 ");
 
 for await (const req of server) {
-	req.respond({ body: "Hello World" });
+	req.respond({ body: "Hello World from deno!" });
 }
