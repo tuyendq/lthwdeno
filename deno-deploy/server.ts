9c382d66cdbb73e0f2613d8bc60f5d84a4e00361
Tuyen Dang <tuyendq@gmail.com>
2020-06-09 10:32:07 +0700

    add deno-deploy

---
+++ deno-deploy/server.ts
@@ -0,0 +1,9 @@
+import { serve } from "https:/deno.land/std@0.55.0/http/server.ts";
+
+const server = serve({ port: 8000 });
+
+console.log("Now serving on port 8000 ");
+
+for await (const req of server) {
+	req.response({ body: "Hello World" });
+}
